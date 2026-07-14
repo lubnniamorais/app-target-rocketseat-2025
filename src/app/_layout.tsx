12de0c2d078f8ff23dbd6962d1fa9625ab3de4a3
lubnniamorais <lubnnia@gmail.com>
2026-07-14 09:40:08 -0300

    feat: Create database connection

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { Stack } from 'expo-router';
 import {
   useFonts,
@@ -5,6 +6,9 @@ import {
   Inter_500Medium,
   Inter_700Bold,
 } from '@expo-google-fonts/inter';
+
+import { SQLiteProvider } from 'expo-sqlite';
+import { migrate } from '@/database/migrate';
 
 import { colors } from '@/theme/color';
 import { Loading } from '@/components/Loading';
@@ -22,13 +26,17 @@ export default function Layout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: {
-          backgroundColor: colors.white,
-        },
-      }}
-    />
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider databaseName='target.db' onInit={migrate} useSuspense>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: colors.white,
+            },
+          }}
+        />
+      </SQLiteProvider>
+    </Suspense>
   );
 }
