7e546374b502a90675bcdd6b79820dd46ebcba2e
lubnniamorais <lubnnia@gmail.com>
2026-07-26 16:54:20 -0300

    chore: Add fucntion to return meta details

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,5 +1,6 @@
-import { View } from 'react-native';
-import { router, useLocalSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { Alert, View } from 'react-native';
+import { router, useFocusEffect, useLocalSearchParams } from 'expo-router';
 
 import { PageHeader } from '@/components/PageHeader';
 import { Progress } from '@/components/Progress';
@@ -7,13 +8,11 @@ import { List } from '@/components/List';
 import { Transaction, TransactionProps } from '@/components/Transaction';
 import { Button } from '@/components/Button';
 
-import { TransactionTypes } from '@/utils/TransactionTypes';
+import { useTargetDatabase } from '@/database/useTargetDatabase';
 
-const details = {
-  current: 'R$ 580,00',
-  target: 'R$ 1.790,00',
-  percentage: 25,
-};
+import { TransactionTypes } from '@/utils/TransactionTypes';
+import { numberToCurrency } from '@/utils/numberToCurrency';
+import { Loading } from '@/components/Loading';
 
 const transactions: TransactionProps[] = [
   {
@@ -32,12 +31,59 @@ const transactions: TransactionProps[] = [
 ];
 
 export default function InProgress() {
+  const [isFetching, setIsFetching] = useState(true);
+  const [details, setDetails] = useState({
+    name: '',
+    current: 'R$ 0,00',
+    target: 'R$ 0,00',
+    percentage: 0,
+  });
   const params = useLocalSearchParams<{ id: string }>();
+
+  const targetDatabase = useTargetDatabase();
+
+  async function fetchDetails() {
+    try {
+      const response = await targetDatabase.show(Number(params.id));
+
+      if (!response) {
+        throw new Error('Meta nao encontrada');
+      }
+
+      setDetails({
+        name: response.name,
+        current: numberToCurrency(response.current),
+        target: numberToCurrency(response.amount),
+        percentage: response.percentage,
+      });
+    } catch (error) {
+      Alert.alert('Erro', 'Nao foi possivel carregar os detalhes da meta');
+      console.log(error);
+    }
+  }
+
+  async function fetchData() {
+    const fetchDetailsPromise = fetchDetails();
+
+    await Promise.all([fetchDetailsPromise]);
+
+    setIsFetching(false);
+  }
+
+  useFocusEffect(() => {
+    useCallback(() => {
+      fetchData();
+    }, []);
+  });
+
+  if (isFetching) {
+    return <Loading />;
+  }
 
   return (
     <View style={{ flex: 1, padding: 24, gap: 32 }}>
       <PageHeader
-        title='Apple Watch'
+        title={details.name}
         rightButton={{
           icon: 'edit',
           onPress: () => {},
@@ -54,7 +100,10 @@ export default function InProgress() {
         emptyMessage='Nenhum transação. Toque em nova transação para guardar seu primeiro dinheiro aqui.'
       />
 
-      <Button title='Nova transação' onPress={() => router.navigate(`/transaction/${params.id}`)} />
+      <Button
+        title='Nova transação'
+        onPress={() => router.navigate(`/transaction/${params.id}`)}
+      />
     </View>
   );
 }
